cc3f2c142cd28c3916bb4aab158ecbc63932906f
Tamilselvan K <tamils1978@gmail.com>
2022-09-21 22:24:18 +0530

    RoadType PO Limit added
    RoadType PO Limit added

--- data-process-ui/src/app/modules/project-management/roadtype-create/roadtype-create.component.ts
+++ data-process-ui/src/app/modules/project-management/roadtype-create/roadtype-create.component.ts
@@ -1,15 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import {
+  NgbActiveModal,
+  NgbDatepickerConfig,
+  NgbDateStruct,
+} from '@ng-bootstrap/ng-bootstrap';
+
+import { AuthService, UserModel } from '../../auth';
+import { ProjectService } from '../../auth/_services/project.services';
 
 @Component({
   selector: 'app-roadtype-create',
   templateUrl: './roadtype-create.component.html',
-  styleUrls: ['./roadtype-create.component.scss']
+  styleUrls: ['./roadtype-create.component.scss'],
 })
 export class RoadtypeCreateComponent implements OnInit {
+  @Input() id: string;
+  isLoading$;
+  isAdminRole: boolean;
+  customer: UserModel;
+  formGroup: FormGroup;
+  minDate: NgbDateStruct;
+  maxDate: NgbDateStruct;
 
-  constructor() { }
+  constructor(
+    public modal: NgbActiveModal,
+    private config: NgbDatepickerConfig,
+    private projectService: ProjectService,
+    private authService: AuthService
+  ) {
+    this.customer = new UserModel();
+    const current = new Date();
+    console.log(current.getFullYear(), current.getMonth(), current.getDate());
+    this.minDate = {
+      year: 2000,
+      month: 1,
+      day: 1,
+    };
+    this.maxDate = {
+      year: current.getFullYear() - 18,
+      month: current.getMonth() + 1,
+      day: current.getDate(),
+    };
 
-  ngOnInit(): void {
+    this.formGroup = new FormGroup({
+      poNumberId: new FormControl(),
+      clientName: new FormControl(),
+      poDate: new FormControl(),
+      paLimit: new FormControl(),
+      poNumber: new FormControl(),
+    });
   }
 
+  ngOnInit(): void {}
+
+  save() {
+    var invalid = this.findInvalidControls();
+    var isValid = false; //invalid.length>0?false:true;
+    if (isValid) {
+      if (this.customer.id) {
+        /*  this.prepareCustomer("Edit");
+        this.edit();*/
+      } else {
+        /*this.prepareCustomer("Create");
+        this.create();*/
+      }
+    } else {
+      alert('Please add valid values for ' + invalid);
+    }
+  }
+  findInvalidControls() {
+    throw new Error('Method not implemented.');
+  }
+  isControlValid(controlName: string): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.valid && (control.dirty || control.touched);
+  }
+
+  isControlInvalid(controlName: string): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.invalid && (control.dirty || control.touched);
+  }
+
+  controlHasError(validation, controlName): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.hasError(validation) && (control.dirty || control.touched);
+  }
+
+  isControlTouched(controlName): boolean {
+    const control = this.formGroup.controls[controlName];
+    return control.dirty || control.touched;
+  }
 }
