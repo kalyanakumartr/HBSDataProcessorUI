99a17edd602326787faecbecc252195c3a309615
Tamilselvan K <tamils1978@gmail.com>
2022-10-13 11:12:41 +0530

    Dual List box Sample in Subcountry
    Dual List box Sample in Subcountry

--- data-process-ui/src/app/modules/project-management/project-assign-subcountry/project-assign-subcountry.component.ts
+++ data-process-ui/src/app/modules/project-management/project-assign-subcountry/project-assign-subcountry.component.ts
@@ -1,19 +1,113 @@
 import { Component, IterableDiffers, OnInit } from '@angular/core';
 import { yearsPerRow } from '@angular/material/datepicker';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { HttpClient } from '@angular/common/http';
 import { DualListComponent } from 'angular-dual-listbox';
+
+
 
 @Component({
   selector: 'app-project-assign-subcountry',
   templateUrl: './project-assign-subcountry.component.html',
   styleUrls: ['./project-assign-subcountry.component.scss']
 })
-export class ProjectAssignSubcountryComponent extends DualListComponent implements OnInit {
+export class ProjectAssignSubcountryComponent implements OnInit {
+  tab = 1;
+  keepSorted = true;
+  key: string;
+  display: string;
+  filter = false;
+  source: Array<any>;
+  confirmed: Array<any>;
+  userAdd = '';
+  disabled = false;
 
-  constructor( differs: IterableDiffers) {
-    super(differs);
+  sourceLeft = true;
+  format: any = DualListComponent.DEFAULT_FORMAT;
+
+  private sourceStations: Array<any>;
+
+  private confirmedStations: Array<any>;
+
+
+  private stations: Array<any> = [
+    { key: 1, station: 'Antonito', state: 'CO' },
+    { key: 2, station: 'Big Horn', state: 'NM' },
+    { key: 3, station: 'Sublette', state: 'NM' },
+    { key: 4, station: 'Toltec', state: 'NM' },
+    { key: 5, station: 'Osier', state: 'CO' },
+    { key: 6, station: 'Chama', state: 'NM' },
+    { key: 7, station: 'Monero', state: 'NM' },
+    { key: 8, station: 'Lumberton', state: 'NM' },
+    { key: 9, station: 'Duice', state: 'NM' },
+    { key: 10, station: 'Navajo', state: 'NM' },
+    { key: 11, station: 'Juanita', state: 'CO' },
+    { key: 12, station: 'Pagosa Jct', state: 'CO' },
+    { key: 13, station: 'Carracha', state: 'CO' },
+    { key: 14, station: 'Arboles', state: 'CO' },
+    { key: 15, station: 'Solidad', state: 'CO' },
+    { key: 16, station: 'Tiffany', state: 'CO' },
+    { key: 17, station: 'La Boca', state: 'CO' },
+    { key: 18, station: 'Ignacio', state: 'CO' },
+    { key: 19, station: 'Oxford', state: 'CO' },
+    { key: 20, station: 'Florida', state: 'CO' },
+    { key: 21, station: 'Bocea', state: 'CO' },
+    { key: 22, station: 'Carbon Jct', state: 'CO' },
+    { key: 23, station: 'Durango', state: 'CO' },
+    { key: 24, station: 'Home Ranch', state: 'CO' },
+    { key: 25, station: 'Trimble Springs', state: 'CO' },
+    { key: 26, station: 'Hermosa', state: 'CO' },
+    { key: 27, station: 'Rockwood', state: 'CO' },
+    { key: 28, station: 'Tacoma', state: 'CO' },
+    { key: 29, station: 'Needleton', state: 'CO' },
+    { key: 30, station: 'Elk Park', state: 'CO' },
+    { key: 31, station: 'Silverton', state: 'CO' },
+    { key: 32, station: 'Eureka', state: 'CO' }
+  ];
+
+
+  ngOnInit() {
+    this.doReset();
   }
 
-  ngOnInit(): void {
+  private useStations() {
+    this.key = 'key';
+    this.display = 'station'; // [ 'station', 'state' ];
+    this.keepSorted = true;
+    this.source = this.sourceStations;
+    this.confirmed = this.confirmedStations;
+  }
+  doReset() {
+    this.sourceStations = JSON.parse(JSON.stringify(this.stations));
+
+    this.confirmedStations = new Array<any>();
+
+    // Preconfirm some items.
+    this.confirmedStations.push(this.stations[31]);
+    this.confirmedStations.push(this.stations[30]);
+    this.confirmedStations.push(this.stations[29]);
+
+    this.useStations();
+  }
+
+
+
+  filterBtn() {
+    return (this.filter ? 'Hide Filter' : 'Show Filter');
+  }
+
+  doDisable() {
+    this.disabled = !this.disabled;
+  }
+
+  disableBtn() {
+    return (this.disabled ? 'Enable' : 'Disabled');
+  }
+
+  swapDirection() {
+    this.sourceLeft = !this.sourceLeft;
+    this.format.direction = this.sourceLeft ? DualListComponent.LTR : DualListComponent.RTL;
   }
 
 }
